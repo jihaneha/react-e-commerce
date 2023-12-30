f679e0c1ef8542804b0ecbf2a06a63519ecc9940
jihaneha <ghamidou800@gmail.com>
2023-12-30 10:14:05 +0100

    first commit

---
+++ src/components/header/Header2.jsx
@@ -0,0 +1,11 @@
+
+
+const Header2 = () => {
+    return (
+        <div>
+            Header2
+        </div>
+    );
+};
+
+export default Header2;
---
+++ src/components/header/Header3.jsx
@@ -0,0 +1,11 @@
+
+
+const Header3 = () => {
+    return (
+        <div>
+            Header3
+        </div>
+    );
+};
+
+export default Header3;
